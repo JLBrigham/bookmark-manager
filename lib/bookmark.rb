1f5c0f9b9152a1228edd4369307d121f9898c30a
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-02-25 16:17:05 +0100

    reverting back 2 commits

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -12,6 +12,23 @@ class Bookmark
   end
   
   def self.all
+    result = get_connection.exec "SELECT * FROM bookmarks"
+    result.map { |row| Bookmark.new(id: row['id'], url: row['url'], title: row['title']) }
+  end
+
+  def self.delete(id:)
+    get_connection.exec("DELETE FROM bookmarks WHERE id = '#{id}'")
+  end
+
+  def initialize(id:, url:, title:)
+    @id = id
+    @url = url
+    @title = title
+  end
+
+  private
+
+  def self.get_connection
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect :dbname => 'bookmark_manager_test'
     else
--- spec/features/add_new_bookmark_spec.rb
+++ spec/features/add_new_bookmark_spec.rb
@@ -1,9 +1,7 @@
 feature 'adding a new bookmark' do
   scenario 'it should allow me to add a new bookmark page' do
-    visit '/'
-    fill_in 'url', with: 'www.bbc.co.uk'
-    click_button 'Submit'
-    click_link 'View Bookmarks'
-    expect(page).to have_content 'www.bbc.co.uk'
+    visit('/bookmarks')
+    adds_a_bookmark
+    expect(page).to have_content 'The Beeb'
   end
 end
